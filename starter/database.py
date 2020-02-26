0cb31c5920da209dcf1bf89825a3d7ba62c1ff6a
Srinivas Saketh <ssaketh97@gmail.com>
2020-02-26 23:05:49 +0530

    Solved Requirement 4. The Code changes for this not much as compared to requirement2 and requiremnt3 in fact at requiement2 itself i have adde all necessary code in filter function in search file The result for the search requirment is present in data/result.csv file. Also in this commit i have added database.py file which now is under pep* guidelines.

--- starter/database.py
+++ starter/database.py
@@ -5,26 +5,28 @@ class NEODatabase(object):
     """
     Object to hold Near Earth Objects and their orbits.
 
-    To support optimized date searching, a dict mapping of all orbit date paths to the Near Earth Objects
-    recorded on a given day is maintained. Additionally, all unique instances of a Near Earth Object
-    are contained in a dict mapping the Near Earth Object name to the NearEarthObject instance.
+    To support optimized date searching, a dict mapping of all orbit date paths
+    to the Near Earth Objectsrecorded on a given day is maintained.
+    Additionally, all unique instances of a Near Earth Object are contained in
+    a dict mapping the Near Earth Object name to the NearEarthObject instance.
     """
 
     def __init__(self, filename):
         """
-        :param filename: str representing the pathway of the filename containing the Near Earth Object data
+        :param filename: str representing the pathway of the filename
+        containing the Near Earth Object data
         """
         self.filename = filename
-        self.orbits  = {}
+        self.orbits = {}
         self.neos = {}
-        # TODO: What data structures will be needed to store the NearEarthObjects and OrbitPaths?
-        # TODO: Add relevant instance variables for this.
 
     def load_data(self, filename=None):
         """
-        Loads data from a .csv file, instantiating Near Earth Objects and their OrbitPaths by:
+        Loads data from a .csv file, instantiating Near Earth Objects
+        and their OrbitPaths by:
            - Storing a dict of orbit date to list of NearEarthObject instances
-           - Storing a dict of the Near Earth Object name to the single instance of NearEarthObject
+           - Storing a dict of the Near Earth Object name to
+           the single instance of NearEarthObject
 
         :param filename:
         :return:
@@ -35,26 +37,31 @@ class NEODatabase(object):
 
         filename = filename or self.filename
         try:
-            neo_data_file = open(filename,'r')
+            neo_data_file = open(filename, 'r')
             columns_in_data = neo_data_file.readline().split(',')
             line_by_line = neo_data_file.readlines()
             for line in line_by_line:
                 data = line.split(',')
-                data_dict = {columns_in_data[i]: data[i] for i in range(len(columns_in_data))}
+                data_dict = {columns_in_data[i]: data[i] for i in range(
+                                                        len(columns_in_data))}
                 if(data_dict['name'] in self.neos.keys()):
                     orbit = OrbitPath(**data_dict)
                     self.neos[orbit.neo_name].update_orbits(orbit)
                     if(data_dict['close_approach_date'] in self.orbits.keys()):
-                        self.orbits[data_dict['close_approach_date']].append(orbit.neo_name)
+                        self.orbits[data_dict['close_approach_date']].append(
+                                                                orbit.neo_name)
                     else:
-                        self.orbits[orbit.close_approach_date] = [orbit.neo_name]
+                        self.orbits[orbit.close_approach_date] = [
+                                                                orbit.neo_name]
                 else:
                     neo = NearEarthObject(**data_dict)
                     self.neos[neo.name] = neo
                     if(data_dict['close_approach_date'] in self.orbits.keys()):
-                        self.orbits[data_dict['close_approach_date']].append(neo.name)
+                        self.orbits[data_dict['close_approach_date']].append(
+                                                                      neo.name)
                     else:
-                        self.orbits[data_dict['close_approach_date']] = [neo.name]
+                        self.orbits[data_dict['close_approach_date']] = [
+                                                                    neo.name]
         finally:
             neo_data_file.close()
             # Next step to create dicts to store orbits,neo objects
